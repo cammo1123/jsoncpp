a915910446eff97fc62c6ed99d1e83cdb8893157
Cameron Youell <cameronyouell@gmail.com>
2022-05-03 17:24:38 +1000

    add premake

---
+++ premake5.lua
@@ -0,0 +1,21 @@
+project "jsoncpp"
+	kind "StaticLib"
+	language "C++"
+
+	targetdir ("%{wks.location}/bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("%{wks.location}/bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files {
+		"src/lib_json/**.cpp",
+		"src/lib_json/**.h",
+	}
+
+	includedirs	{
+		"include",
+	}
+	
+	filter "configurations:Debug"
+		symbols "On"
+
+	filter "configurations:Release"
+		optimize "On"
